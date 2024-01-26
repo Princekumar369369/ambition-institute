c76e7ef479a3ee11d1555a28c1e03df1c8f97c3b
Princekumar369369 <princekumar369369369@gmail.com>
2024-01-26 11:20:38 +0530

    Remove unneccery files form the admin panel

--- php-ajax/config.php
+++ php-ajax/config.php
@@ -1,5 +1,6 @@
 <?php 
  $conn = mysqli_connect("localhost","root","","ambition_db");
+//  $conn = mysqli_connect("sql212.infinityfree.com","if0_35861988","princekumar2002","if0_35861988_ambition_db");
 
 
 ?>
